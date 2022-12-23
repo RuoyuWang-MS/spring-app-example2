b7a5167c460db3b20fb2d5f4d842337125302d10
Ruoyu Wang <ruoyuwang@microsoft.com>
2022-12-23 15:15:18 +0800

    try new workflows

---
+++ .github/workflows/create_service_app.sh
@@ -0,0 +1,6 @@
+set -ex
+
+az account set -s $subscription
+az group create -l $location -n $rg_name
+az spring create -g $rg_name -n $service_name
+az spring app create -n $app_name -g $rg_name -s $service_name
